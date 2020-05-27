e36b26dcc676aeaabc869b3b3eee1da79d88c827
lilu <admin@lilu.org.cn>
2020-05-27 21:24:26 +0800

    add LinkedListTest and clone test

---
+++ java-se/src/main/java/com/sunchaser/javase/collect/JavaCollectionTest.java
@@ -0,0 +1,12 @@
+package com.sunchaser.javase.collect;
+
+/**
+ * @author sunchaser
+ * @date 2020/5/9
+ * @since 1.0
+ */
+public class JavaCollectionTest {
+
+    public static void main(String[] args) {
+    }
+}
