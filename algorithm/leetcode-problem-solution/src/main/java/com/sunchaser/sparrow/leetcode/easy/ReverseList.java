c05a70579d5bad90eaf327680ef3bdf190f59979
SunChaser <admin@lilu.org.cn>
2020-12-02 23:31:09 +0800

    add IntReverse.java and ThreadTest.java

--- algorithm/leetcode-problem-solution/src/main/java/com/sunchaser/sparrow/leetcode/easy/ReverseList.java
+++ algorithm/leetcode-problem-solution/src/main/java/com/sunchaser/sparrow/leetcode/easy/ReverseList.java
@@ -14,7 +14,7 @@ package com.sunchaser.sparrow.leetcode.easy;
  * 链接：https://leetcode-cn.com/problems/reverse-linked-list
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
- * @author sunchaser
+ * @author sunchaser admin@lilu.org.cn
  * @since JDK8 2020/6/2
  */
 public class ReverseList {
