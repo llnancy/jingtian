38c526a70ac6c58f16c2d881bf58b46515b6fcab
SunChaser <admin@lilu.org.cn>
2020-12-13 04:25:39 +0800

    add @Slf4j test

---
+++ java-ee/lombok/src/main/java/org/sunchaser/lombok/test/LogTest.java
@@ -0,0 +1,11 @@
+package org.sunchaser.lombok.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author sunchaser admin@lilu.org.cn
+ * @since JDK8 2020/12/13
+ */
+@Slf4j
+public class LogTest {
+}
