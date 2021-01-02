c5b79d6647309774f460978f50abeb571c0eef99
sunchaser-lilu <admin@lilu.org.cn>
2021-01-03 01:36:23 +0800

    add PalindromeLinkedList.java

--- algorithm/leetcode-problem-solution/src/main/java/com/sunchaser/sparrow/leetcode/tag/linkedlist/easy/PalindromeLinkedList.java
+++ algorithm/leetcode-problem-solution/src/main/java/com/sunchaser/sparrow/leetcode/tag/linkedlist/easy/PalindromeLinkedList.java
@@ -15,23 +15,87 @@ public class PalindromeLinkedList {
 
     public static void main(String[] args) {
         SinglyLinkedListNode head = LinkedListUtils.generateSinglyLinkedList();
-        boolean noPalindrome = isPalindrome(head);
+        boolean noPalindrome = isPalindromeUseArray(head);
+        System.out.println(head);
         System.out.println(noPalindrome);
+        boolean noPalindromeUseReverse = isPalindromeUseReverse(head);
+        System.out.println(head);
+        System.out.println(noPalindromeUseReverse);
+        SinglyLinkedListNode optHead = LinkedListUtils.generateSinglyLinkedList();
+        boolean noPalindromeUseReverseOptimization = isPalindromeUseReverseOptimization(optHead);
+        System.out.println(optHead);
+        System.out.println(noPalindromeUseReverseOptimization);
         SinglyLinkedListNode palindromeHead = LinkedListUtils.generatePalindromeLinkedList();
-        boolean palindrome = isPalindrome(palindromeHead);
+        boolean palindrome = isPalindromeUseArray(palindromeHead);
+        System.out.println(head);
         System.out.println(palindrome);
+        boolean palindromeUseReverse = isPalindromeUseReverse(palindromeHead);
+        System.out.println(head);
+        System.out.println(palindromeUseReverse);
+        SinglyLinkedListNode optPalindromeHead = LinkedListUtils.generatePalindromeLinkedList();
+        boolean palindromeUseReverseOptimization = isPalindromeUseReverseOptimization(optPalindromeHead);
+        System.out.println(head);
+        System.out.println(palindromeUseReverseOptimization);
     }
 
-    public static boolean isPalindrome(SinglyLinkedListNode head) {
+    public static boolean isPalindromeUseArray(SinglyLinkedListNode head) {
         List<SinglyLinkedListNode> list = new ArrayList<>();
         while (head != null) {
             list.add(head);
             head = head.next;
         }
-        for (int i = 0; i < list.size(); i++) {
-            if (!list.get(i).val.equals(list.get(list.size() - i - 1).val)) {
-                return false;
-            }
+        for (int i = 0,size = list.size();i < size / 2;i++) {
+            if (!list.get(i).val.equals(list.get(size - i - 1).val)) return false;
+        }
+        return true;
+    }
+
+    public static boolean isPalindromeUseReverse(SinglyLinkedListNode head) {
+        if (head == null || head.next == null) return true;
+        SinglyLinkedListNode fast = head;
+        SinglyLinkedListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        SinglyLinkedListNode p1 = head;
+        SinglyLinkedListNode p2 = slow;
+        SinglyLinkedListNode pre = slow;
+        SinglyLinkedListNode next;
+        while (p1 != p2) {
+            next = p1.next;
+            p1.next = pre;
+            pre = p1;
+            p1 = next;
+        }
+        if (fast != null) p2 = p2.next;
+        while (p2 != null) {
+            if (!pre.val.equals(p2.val)) return false;
+            pre = pre.next;
+            p2 = p2.next;
+        }
+        return true;
+    }
+
+    public static boolean isPalindromeUseReverseOptimization(SinglyLinkedListNode head) {
+        if (head == null || head.next == null) return true;
+        SinglyLinkedListNode fast = head;
+        SinglyLinkedListNode slow = head;
+        SinglyLinkedListNode pre = null;
+        SinglyLinkedListNode next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            next = slow.next;
+            slow.next = pre;
+            pre = slow;
+            slow = next;
+        }
+        if (fast != null) slow = slow.next;
+        while (slow != null) {
+            assert pre != null;
+            if (!pre.val.equals(slow.val)) return false;
+            pre = pre.next;
+            slow = slow.next;
         }
         return true;
     }
