a2fee12379a730fbb46558112b5657a975112a9b
sunchaser-lilu <admin@lilu.org.cn>
2021-02-02 13:39:52 +0800

    fix StopWatchWrapper.java

--- java-ee/springboot/src/main/java/com/sunchaser/sparrow/springboot/utils/StopWatchWrapper.java
+++ java-ee/springboot/src/main/java/com/sunchaser/sparrow/springboot/utils/StopWatchWrapper.java
@@ -54,14 +54,14 @@ public class StopWatchWrapper extends StopWatch implements AutoCloseable {
         if (this.isRunning()) {
             this.stop();
         }
-        if (countDownLatch != null) {
-            countDownLatch.countDown();
-        }
     }
 
     @Override
     public void stop() throws IllegalStateException {
         super.stop();
+        if (countDownLatch != null) {
+            countDownLatch.countDown();
+        }
         if (StringUtils.isNotBlank(this.getId())) {
             log.info("{}-执行耗时{}ms", this.getId(), this.getTotalTimeMillis());
         }
