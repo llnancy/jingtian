95f36fdde3381e01bde93ae3b37d4e97700d8ac4
sunchaser-lilu <admin@lilu.org.cn>
2021-02-08 03:24:43 +0800

    add ribbon module

--- microservice/spring-cloud/ribbon/src/main/java/com/sunchaser/sparrow/microservice/springcloud/ribbon/RibbonApplication.java
+++ microservice/spring-cloud/ribbon/src/main/java/com/sunchaser/sparrow/microservice/springcloud/ribbon/RibbonApplication.java
@@ -1,24 +1,26 @@
-package com.sunchaser.sparrow.microservice.springcloud.eureka.client.consumer;
+package com.sunchaser.sparrow.microservice.springcloud.ribbon;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * @author sunchaser admin@lilu.org.cn
- * @since JDK8 2021/2/5
+ * @since JDK8 2021/2/8
  */
 @SpringBootApplication
-public class EurekaClientConsumerApplication {
+public class RibbonApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EurekaClientConsumerApplication.class)
+        new SpringApplicationBuilder(RibbonApplication.class)
                 .web(WebApplicationType.SERVLET)
                 .run(args);
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
