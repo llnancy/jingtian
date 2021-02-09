c7e807525b5b9ea3ac4a40254405be1198697c92
sunchaser-lilu <admin@lilu.org.cn>
2021-02-09 21:49:43 +0800

    add ribbon loadBalancerClient test

--- microservice/spring-cloud/ribbon/src/main/java/com/sunchaser/sparrow/microservice/springcloud/ribbon/controller/ConsumerController.java
+++ microservice/spring-cloud/ribbon/src/main/java/com/sunchaser/sparrow/microservice/springcloud/ribbon/controller/ConsumerController.java
@@ -1,6 +1,10 @@
 package com.sunchaser.sparrow.microservice.springcloud.ribbon.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,19 +16,21 @@ import org.springframework.web.client.RestTemplate;
  */
 @RestController
 public class ConsumerController {
+    private static final Logger log = LoggerFactory.getLogger(ConsumerController.class);
     @Autowired
     private RestTemplate restTemplate;
-//
-//    @Autowired
-//    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
 
     @GetMapping("/consumer/{id}")
     public String consume(@PathVariable String id) {
         return restTemplate.getForObject("http://localhost:9000/provider?id=" + id, String.class);
     }
-//
-//    @GetMapping("/provider/instance")
-//    public List<ServiceInstance> getProviderInstanceList() {
-//        return discoveryClient.getInstances("eureka-client-service-provider");
-//    }
+
+    @GetMapping("/print/provider/instance")
+    public void getProviderInstanceList() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("eureka-client-service-provider");
+        log.info("service instance:{}", serviceInstance);
+    }
 }
