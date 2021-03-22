db70ea79bff96d37f3dfe0d05978789e3aef327e
sunchaser-lilu <admin@lilu.org.cn>
2021-03-22 11:55:22 +0800

    fix dubbo sample

---
+++ microservice/dubbo-samples/dubbo-xml/dubbo-xml-consumer/src/main/java/com/sunchaser/sparrow/microservice/dubbo/xml/consumer/web/controller/EchoController.java
@@ -0,0 +1,12 @@
+package com.sunchaser.sparrow.microservice.dubbo.xml.consumer.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sunchaser admin@lilu.org.cn
+ * @since JDK8 2021/3/19
+ */
+@RestController
+public class EchoController {
+
+}
