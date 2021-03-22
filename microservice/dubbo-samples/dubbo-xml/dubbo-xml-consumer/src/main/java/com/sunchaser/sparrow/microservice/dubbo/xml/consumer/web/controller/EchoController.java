9e4ce43559e5f6a125e5fb3fe72c0aef24bd2631
sunchaser-lilu <admin@lilu.org.cn>
2021-03-22 23:10:47 +0800

    fix dubbo sample

--- microservice/dubbo-samples/dubbo-xml/dubbo-xml-consumer/src/main/java/com/sunchaser/sparrow/microservice/dubbo/xml/consumer/web/controller/EchoController.java
+++ microservice/dubbo-samples/dubbo-xml/dubbo-xml-consumer/src/main/java/com/sunchaser/sparrow/microservice/dubbo/xml/consumer/web/controller/EchoController.java
@@ -1,6 +1,10 @@
 package com.sunchaser.sparrow.microservice.dubbo.xml.consumer.web.controller;
 
+import com.sunchaser.sparrow.microservice.dubbo.facade.EchoFacade;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author sunchaser admin@lilu.org.cn
@@ -8,5 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class EchoController {
+    @Resource
+    private EchoFacade echoFacade;
 
+    @GetMapping("echo")
+    public String echo(String msg) {
+        return echoFacade.echo(msg);
+    }
 }
