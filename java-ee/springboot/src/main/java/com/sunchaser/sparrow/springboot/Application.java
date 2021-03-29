e5a96017ecacc8c4bcd6be32b39e74688085be23
sunchaser-lilu <admin@lilu.org.cn>
2021-03-29 16:30:07 +0800

    reset and fix

--- java-ee/springboot/src/main/java/com/sunchaser/sparrow/springboot/Application.java
+++ java-ee/springboot/src/main/java/com/sunchaser/sparrow/springboot/Application.java
@@ -2,6 +2,8 @@ package com.sunchaser.sparrow.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author sunchaser
@@ -11,5 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
