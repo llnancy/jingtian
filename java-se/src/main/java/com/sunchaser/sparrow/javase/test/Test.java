4fda74b48e5729677601b3e79ec57d82885613e4
lilu <admin@lilu.org.cn>
2022-04-20 21:45:23 +0800

    [update] update Test.java

--- java-se/src/main/java/com/sunchaser/sparrow/javase/test/Test.java
+++ java-se/src/main/java/com/sunchaser/sparrow/javase/test/Test.java
@@ -1,22 +1,16 @@
 package com.sunchaser.sparrow.javase.test;
 
+import java.util.Random;
+
 /**
  * @author sunchaser
  * @since JDK8 2019/12/5
- *
  */
 public class Test {
-    public static void main(String[] args) {
-        System.out.println(tryFinally());
-    }
 
-    private static int tryFinally() {
-        try {
-            System.out.println("try...");
-            System.exit(0);
-            return 1;
-        } finally {
-            System.out.println("finally...");
-        }
+    public static void main(String[] args) {
+        Random random = new Random();
+        int nextInt = random.nextInt(10);
+        System.out.println(nextInt);
     }
 }
