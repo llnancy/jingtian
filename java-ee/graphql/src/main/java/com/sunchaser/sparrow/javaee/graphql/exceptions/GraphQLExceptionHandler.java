f5259ca0a39a885fb0bc4494860406c358c51843
lilu <admin@lilu.org.cn>
2022-05-11 21:20:00 +0800

    [docs] add a comment

--- java-ee/graphql/src/main/java/com/sunchaser/sparrow/javaee/graphql/exceptions/GraphQLExceptionHandler.java
+++ java-ee/graphql/src/main/java/com/sunchaser/sparrow/javaee/graphql/exceptions/GraphQLExceptionHandler.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.validation.ConstraintViolationException;
 
 /**
+ * 全局异常处理器
+ *
  * @author sunchaser admin@lilu.org.cn
+ * @see graphql.kickstart.execution.error.DefaultGraphQLErrorHandler
  * @since JDK8 2022/5/6
  */
 @Component
