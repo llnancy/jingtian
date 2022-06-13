81d0c4e181e5930bb08087834966a7ff1047c4de
lilu <admin@lilu.org.cn>
2022-06-13 18:09:33 +0800

    [style] line

--- java-se/src/main/java/com/sunchaser/sparrow/javase/shell/ProcessBuilderTest.java
+++ java-se/src/main/java/com/sunchaser/sparrow/javase/shell/ProcessBuilderTest.java
@@ -14,6 +14,7 @@ import java.util.Map;
  */
 @Slf4j
 public class ProcessBuilderTest {
+
     public static void main(String[] args) throws IOException, InterruptedException {
         ProcessBuilder builder = new ProcessBuilder("/bin/sh", "-c", "curl https://lilu.org.cn/");
 
