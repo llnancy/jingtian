45ee49ac3c8c4aa1b4bce7953bb37f1e3f870d05
lilu <admin@lilu.org.cn>
2022-10-09 12:04:21 +0800

    feat: update thread pool

--- java-se/src/main/java/com/sunchaser/sparrow/javase/concurrency/ThreadPool.java
+++ java-se/src/main/java/com/sunchaser/sparrow/javase/concurrency/ThreadPool.java
@@ -1,6 +1,17 @@
 package com.sunchaser.sparrow.javase.concurrency;
 
-import java.util.concurrent.*;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * thread pool
@@ -12,16 +23,26 @@ public class ThreadPool {
 
     public static void main(String[] args) {
 
+        // 默认线程工厂
+        ThreadFactory defaultTf = Executors.defaultThreadFactory();
+
+        // Guava 线程工厂
+        ThreadFactory threadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("biz-pool-%s-thread-%s")
+                .build();
+
+        // 手动创建线程池
         ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
                 2,
                 2,
                 60,
                 TimeUnit.SECONDS,
                 new LinkedBlockingQueue<>(1000),
-                Executors.defaultThreadFactory(),
+                threadFactory,
                 new ThreadPoolExecutor.CallerRunsPolicy()
         );
 
+        // 执行无返回值任务
         threadPool.execute(new Runnable() {
             @Override
             public void run() {
@@ -29,6 +50,7 @@ public class ThreadPool {
             }
         });
 
+        // 提交有返回值任务
         Future<String> future = threadPool.submit(new Callable<String>() {
             @Override
             public String call() throws Exception {
@@ -36,9 +58,8 @@ public class ThreadPool {
             }
         });
 
-        threadPool.shutdown();
-
         try {
+            // 从 Future 中获取返回值
             String res = future.get();
         } catch (InterruptedException e) {
             // 处理中断异常
@@ -49,16 +70,30 @@ public class ThreadPool {
             throw new RuntimeException(e);
         }
 
+        // 关闭线程池
+        threadPool.shutdown();
+
+        // 预热一个核心线程
+        threadPool.prestartCoreThread();
+        // 预热全部核心线程
+        threadPool.prestartAllCoreThreads();
+
+        // 仅包含一个线程的线程池
         ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
 
+        // 固定2个线程的线程池
         ExecutorService fixedThreadPool = Executors.newFixedThreadPool(2);
 
+        // 线程数不限的线程池
         ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
 
-        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(2);
-
+        // 仅包含一个线程的 ScheduledThreadPoolExecutor 线程池
         ScheduledExecutorService singleThreadScheduledExecutor = Executors.newSingleThreadScheduledExecutor();
 
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        // 固定2个线程数的 ScheduledThreadPoolExecutor 线程池
+        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(2);
+
+        // fork join pool
+        ExecutorService forkJoinPool = Executors.newWorkStealingPool();
     }
 }
