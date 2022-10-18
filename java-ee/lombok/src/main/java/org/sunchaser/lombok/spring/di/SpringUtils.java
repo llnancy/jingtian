9bc0d51fcf1836825d6c38a45ce2122ce364d7d6
lilu <admin@lilu.org.cn>
2022-10-18 21:13:15 +0800

    fix: remove unuseful code

--- java-ee/lombok/src/main/java/org/sunchaser/lombok/spring/di/SpringUtils.java
+++ java-ee/lombok/src/main/java/org/sunchaser/lombok/spring/di/SpringUtils.java
@@ -5,18 +5,17 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-import javax.validation.constraints.NotNull;
-
 /**
  * @author sunchaser admin@lilu.org.cn
  * @since JDK8 2020/12/13
  */
 @Component
 public class SpringUtils implements ApplicationContextAware {
+
     private static ApplicationContext applicationContext;
 
     @Override
-    public void setApplicationContext(@NotNull ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         if (SpringUtils.applicationContext == null) {
             SpringUtils.applicationContext = applicationContext;
         }
