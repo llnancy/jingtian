c2b9df9ce64d9e151e339227211e4ccee1532de8
llnancy <admin@lilu.org.cn>
2024-10-10 20:52:44 +0800

    feat: 10.09 - 10.10

--- algorithm/leetcode/src/main/java/io/github/llnancy/jingtian/algorithm/leetcode/hot100/SymmetricTree.java
+++ algorithm/leetcode/src/main/java/io/github/llnancy/jingtian/algorithm/leetcode/hot100/SymmetricTree.java
@@ -3,14 +3,22 @@ package io.github.llnancy.jingtian.algorithm.leetcode.hot100;
 import io.github.llnancy.jingtian.algorithm.common.TreeNode;
 
 /**
- * 对称二叉树
- * <a href="https://leetcode.cn/problems/symmetric-tree/description/?envType=featured-list&envId=2cktkvj?envType=featured-list&envId=2cktkvj">https://leetcode.cn/problems/symmetric-tree/description/?envType=featured-list&envId=2cktkvj?envType=featured-list&envId=2cktkvj</a>
+ * 101. 对称二叉树
+ * <a href="https://leetcode.cn/problems/symmetric-tree/">https://leetcode.cn/problems/symmetric-tree/</a>
  *
  * @author llnancy admin@lilu.org.cn
  * @since JDK8 2023/11/17
  */
 public class SymmetricTree {
 
+    /*
+     * 遍历思路：用一个外部变量记录是否对称；用两个指针遍历二叉树，先序遍历位置判断两个指针是否有值和值是否相等，更新结果。如果对称，则遍历子树，指针一的左子树和指针二的右子树；指针一的右子树和指针二的左子树。
+     * 分解问题思路：一颗二叉树是否对称可由子树是否对称的结果推导出来。
+     */
+
+    /**
+     * 记录是否对称的外部变量
+     */
     boolean res = true;
 
     public boolean isSymmetric(TreeNode root) {
@@ -18,20 +26,49 @@ public class SymmetricTree {
         return res;
     }
 
-    private void traverse(TreeNode left, TreeNode right) {
-        if (left == null && right == null) {
+    private void traverse(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
             return;
         }
-        if (left == null || right == null) {
+        // 先序遍历位置
+        // 判断两个指针是否有值
+        if (p == null || q == null) {
             res = false;
             return;
         }
-        if (left.val != right.val) {
+        // 判断两个指针值是否相等
+        if (p.val != q.val) {
             res = false;
+            return;
         }
         if (res) {
-            traverse(left.left, right.right);
-            traverse(left.right, right.left);
+            traverse(p.left, q.right);
+            traverse(p.right, q.left);
         }
+    }
+
+    public boolean isSymmetricII(TreeNode root) {
+        return isSubSymmetric(root.left, root.right);
+    }
+
+    /**
+     * 定义：输入两棵树的根节点，返回这两个树是否对称
+     */
+    private boolean isSubSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        // 判断两棵树的当前节点是否对称
+        if (left.val != right.val) {
+            return false;
+        }
+        // 利用定义得到 left 的左子树和 right 的右子树是否对称
+        boolean m = isSubSymmetric(left.left, right.right);
+        // 利用定义得到 left 的右子树和 right 的左子树是否对称
+        boolean n = isSubSymmetric(left.right, right.left);
+        return m && n;
     }
 }
